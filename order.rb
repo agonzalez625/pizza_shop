fd5058ad407b4dbe57837b1566e9494876e91ead
alejandra <Alexcristin.lopez@gmail.com>
2017-07-27 09:10:29 -0500

    created menu and order classes

---
+++ order.rb
@@ -0,0 +1,8 @@
+class Order
+  def initialize(first_name, last_name, phone_numer, address)
+    @first_name = first_name
+    @last_name = last_name
+    @phone_numer = phone_numer
+    @address = address
+  end
+end
